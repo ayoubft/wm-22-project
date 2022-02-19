2a0309cdddf54c8c6e0784ace838d9cbc90f56a5
ayoubft <ayoubfatihi1999@gmail.com>
2022-02-19 20:21:56 +0100

    added pg how to connect get data

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
-const pg = require("pg");
+const client = require('./connection.js')
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.listen(3000, () => console.log("Listening at 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
+app.use(bodyParser.json());
 
 app.post("/api", (request, response) => {
   console.log("I got a request !");
   console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: "success",
+    query: data.q,
+  })
 });
+
+app.get('/api', (req, res)=>{
+  client.query("SELECT * FROM testtable", (err, result)=>{
+      if(!err){
+          res.send(result.rows);
+      }
+  });
+  client.end;
+})
+client.connect();
