279672672b81b5c053d7b309c5a87daa7707825e
ayoubft <ayoubfatihi1999@gmail.com>
2022-02-20 22:54:51 +0100

    added feature of adding geom from query to map

--- public/js/script2.js
+++ public/js/script2.js
@@ -1,17 +1,24 @@
 let myquery;
+let json3;
 
-const button = document.getElementById('btngetQuery');
-button.addEventListener('click', async event => {
-  const data = {"q": document.getElementById("searchTxt").value};
-  const options = {
-    method: 'POST',
+const button = document.getElementById("btngetQuery");
+button.addEventListener("click", async (event) => {
+  let data = { q: document.getElementById("searchTxt").value };
+  let options = {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   };
-  const response = await fetch('/api', options);
-  const json = await response.json();
+  let response = await fetch("/api", options);
+  let json = await response.json();
   console.log(json);
-});
 
+  let response2 = await fetch("/api");
+  let json2 = await response2.json();
+  console.log(json2);
+  json2.forEach((i) => {
+    L.geoJSON(JSON.parse(i.geom), { icon: myIcon }).addTo(map);
+  });
+});
