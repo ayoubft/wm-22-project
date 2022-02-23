f69e0cd745f06a1fbcbb64352f0b8c74db12e643
ayoubft <ayoubfatihi1999@gmail.com>
2022-02-23 20:17:55 +0100

    refactored postgres login to connectionString

--- connection.js
+++ connection.js
@@ -1,18 +1,8 @@
 const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   port: 5432,
-//   password: "",
-//   database: process.env.DATABASE,
-// });
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 module.exports = pool;
