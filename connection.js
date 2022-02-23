59099d1e964281e82a6c11431af0c2e4fc7ca8fd
ayoubft <ayoubfatihi1999@gmail.com>
2022-02-23 20:23:45 +0100

    modified connection setup

--- connection.js
+++ connection.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgres://postgres@localhost:5432/wm-22-precip-DB",
+  ssl: process.env.DATABASE_URL ? true : false,
 });
 
 module.exports = pool;
