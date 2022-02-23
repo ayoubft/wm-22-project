2c5d1c8b7f1d7022e91732b23fc427de60c48520
ayoubft <ayoubfatihi1999@gmail.com>
2022-02-23 20:28:33 +0100

    changed ssl connection

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
     "postgres://postgres@localhost:5432/wm-22-precip-DB",
-  ssl: process.env.DATABASE_URL ? true : false,
+  ssl: process.env.SSL_BOOL,
 });
 
 module.exports = pool;
