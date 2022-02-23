2f978d9cead8ce1e7f1417fad9873ba5b0a6b3fb
ayoubft <ayoubfatihi1999@gmail.com>
2022-02-23 20:40:06 +0100

    ssl on

--- connection.js
+++ connection.js
@@ -5,6 +5,9 @@ const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
     "postgres://postgres@localhost:5432/wm-22-precip-DB",
+  ssl: {
+    rejectUnauthorized: process.env.SSL_BOOL,
+  },
 });
 
 module.exports = pool;
