5112677eb305fc81962742400783da40d332d84b
ayoubft <ayoubfatihi1999@gmail.com>
2022-02-23 02:39:40 +0100

    added db of heroku

--- connection.js
+++ connection.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   port: 5432,
+//   password: "",
+//   database: process.env.DATABASE,
+// });
+
 const pool = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  port: 5432,
-  password: "",
-  database: process.env.DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
